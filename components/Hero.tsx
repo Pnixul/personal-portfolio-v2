6c068f690518550b31da66f17978f21b3e4dfa7e
Chitipat <pinxulpon@gmail.com>
2026-09-09 15:01:50 +0700

    chore: initialize portfolio v2

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <section className="min-h-screen">
+      <div>
+        <p>PORTFOLIO / 2026</p>
+
+        <h1>
+          CHITIPAT
+          <br />
+          RITTICHOT
+        </h1>
+
+        <p>
+          Frontend Developer
+        </p>
+      </div>
+    </section>
+  );
+}
